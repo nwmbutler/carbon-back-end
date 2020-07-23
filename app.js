c527d10a2cf3d5aa43eb08878eb02b805f8401e6
Nick Butler <nwmbutler@gmail.com>
2020-07-23 15:52:14 +0100

    removed refs to user route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
 var app = express();
 // view engine setup
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use("/testAPI", testAPIRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
