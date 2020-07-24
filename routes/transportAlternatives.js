90516f67db98a82a09d1df47f8706b84818d31cd
Rich Ewin <richardewinlambert@gmail.com>
2020-07-24 15:38:26 +0100

    Creates API request for bus emissions

---
+++ routes/transportAlternatives.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var axios = require ('axios');
+var busCalculate = require('../modules/busCalculator')
+
+router.post('/', function(req, res) {
+    var journey_data_out = req.body.posted_data
+    // console.log(journey_data_out)
+
+axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
+
+  params: {
+    origins: journey_data_out.origin,
+    destinations: journey_data_out.destination,
+    mode: transit,
+    transit_mode: journey_data_out.transport_mode,
+    key: ''}})
+
+  .then(function (response) {
+    // console.log(response)
+    var google_data_in = response.data.rows[0].elements[0]
+    // console.log(google_data_in)
+    var emissions = busCalculate(google_data_in.distance.value)
+    res.send({ distance: emissions});
+  });
+});
+
+module.exports = router;
