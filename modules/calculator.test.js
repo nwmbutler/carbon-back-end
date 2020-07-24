1415e149cf3f72a8390ade7d4196f1e4cb174ada
J Perrin <perrinjack96@gmail.com>
2020-07-24 11:38:33 +0100

    refactored calculate test to return 0 eissions for bicycling too

--- modules/calculator.test.js
+++ modules/calculator.test.js
@@ -9,3 +9,8 @@ test('returns 0 emissions for walking ', () => {
   var emissions = 0;
   expect(calculate(100, 'walking')).toBe(emissions);
 });
+
+test('returns 0 emissions for bicycling ', () => {
+  var emissions = 0;
+  expect(calculate(100, 'bicycling')).toBe(emissions);
+});
