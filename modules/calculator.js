46f27fa668d09869fe650f1e16e6e9ed361eb060
J Perrin <perrinjack96@gmail.com>
2020-07-24 11:25:20 +0100

    calculator returns emissions for car

---
+++ modules/calculator.js
@@ -0,0 +1,4 @@
+function calculate(a, b) {
+    return 122.1;
+  }
+  module.exports = calculate;
---
+++ modules/calculator.test.js
@@ -0,0 +1,6 @@
+const calculate = require('./calculator');
+
+test('returns emissions for a car', () => {
+  var emissions = 122.1
+  expect(calculate(1000, 'car' )).toBe(emissions);
+});
