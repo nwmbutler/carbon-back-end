844317a8dd7d70027d1f46e27ae300cde8d224c2
J Perrin <perrinjack96@gmail.com>
2020-07-24 11:30:56 +0100

    RED- calculate test returns 0 emissions for walking mode

--- modules/calculator.test.js
+++ modules/calculator.test.js
@@ -1,7 +1,11 @@
 const calculate = require('./calculator');
 
 test('returns emissions for a car', () => {
-  var emissions = 122.1
-  expect(calculate(1000, 'car' )).toBe(emissions);
+  var emissions = 122.1;
+  expect(calculate(1000, 'car')).toBe(emissions);
 });
 
+test('returns 0 emissions for walking ', () => {
+  var emissions = 0;
+  expect(calculate(100, 'car')).toBe(0);
+});
