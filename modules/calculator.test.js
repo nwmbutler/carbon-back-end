c32a95eb3c97b60b6d3d85e8799f948fa1cefa2c
J Perrin <perrinjack96@gmail.com>
2020-07-27 13:53:55 +0100

    adjusted imported file name due to renaming of files

--- modules/calculator.test.js
+++ modules/calculator.test.js
@@ -1,16 +1,16 @@
-const calculate = require('./calculator');
+const carCalculate = require('./carCalculator');
 
 test('returns emissions for a car', () => {
   var emissions = 122.1;
-  expect(calculate(1000, 'driving')).toBe(emissions);
+  expect(carCalculate(1000, 'driving')).toBe(emissions);
 });
 
 test('returns 0 emissions for walking ', () => {
   var emissions = 0;
-  expect(calculate(100, 'walking')).toBe(emissions);
+  expect(carCalculate(100, 'walking')).toBe(emissions);
 });
 
 test('returns 0 emissions for bicycling ', () => {
   var emissions = 0;
-  expect(calculate(100, 'bicycling')).toBe(emissions);
+  expect(carCalculate(100, 'bicycling')).toBe(emissions);
 });
