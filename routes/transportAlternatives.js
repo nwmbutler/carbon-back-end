052639fd8642894843ef910c50b944d28bed1d54
J Perrin <perrinjack96@gmail.com>
2020-07-27 11:48:19 +0100

    Api returns alternative transport calculation

--- routes/transportAlternatives.js
+++ routes/transportAlternatives.js
@@ -6,19 +6,19 @@ var trainCalculate = require('../modules/trainCalculator')
 
 router.post('/', function(req, res) {
     var journey_data_out = req.body.posted_data
-    // console.log(journey_data_out)
+    console.log(journey_data_out)
 
 axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
 
   params: {
     origins: journey_data_out.origin,
     destinations: journey_data_out.destination,
-    mode: transit,
-    transit_mode: journey_data_out.transport_mode,
-    key: ''}})
+    mode: 'transit',
+    transit_mode: journey_data_out.transit_mode,
+    key: 'AIzaSyAmrtb43RG-jCR4qVIVAKZQbXJjvn4GnLw'}})
 
   .then(function (response) {
-    // console.log(response)
+    console.log(response)
     var google_data_in = response.data.rows[0].elements[0]
     // console.log(google_data_in)
     if (req.body.posted_data.transit_mode == 'bus') {
