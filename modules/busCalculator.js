44d83be80c5c0214e416f36f629fe55436610616
Nikita <latinisnotdead07@gmail.com>
2020-07-27 18:03:46 +0100

    Added units to emission

--- modules/busCalculator.js
+++ modules/busCalculator.js
@@ -1,7 +1,7 @@
 function busCalculate(distance) {
     var raw_emission = distance * (0.02779 / 1000);
     var emission = Math.round(raw_emission)
-    return `${emission}`
+    return `${emission} kg CO2e`
   }
 module.exports = busCalculate;
 
