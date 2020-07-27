3d994bb11ca13094fbfe953819037e064bcf1ac5
Nikita <latinisnotdead07@gmail.com>
2020-07-27 17:54:00 +0100

    Change calculation constant to kg/km and added source comment

--- modules/carCalculator.js
+++ modules/carCalculator.js
@@ -2,7 +2,7 @@ const e = require("express");
 
 function carCalculate(distance, mode) {
   if (mode == 'driving') {
-    var raw_emission = distance * (192.28 / 1000);
+    var raw_emission = distance * (0.19228 / 1000);
     emission = Math.round(raw_emission)
     return `${emission}`
   } else {
@@ -10,3 +10,5 @@ function carCalculate(distance, mode) {
   }
 }
 module.exports = carCalculate;
+
+// 0.19228 kg CO2e /km Passenger Vehicles, Cars(by size), Medium car, petrol
