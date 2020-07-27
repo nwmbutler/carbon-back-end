5910c58e794f21859225eeaa1bded4a3e243649d
Nikita <latinisnotdead07@gmail.com>
2020-07-27 18:04:02 +0100

    Added units to emission

--- modules/trainCalculator.js
+++ modules/trainCalculator.js
@@ -1,7 +1,7 @@
 function trainCalculate(distance) {
     var raw_emission = distance * (0.04115 / 1000);
     var emission = Math.round(raw_emission);
-    return `${emission}`
+    return `${emission} kg CO2e`
   }
 module.exports = trainCalculate;
 
