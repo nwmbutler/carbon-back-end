dcf82ec3b0a8337e7cfbf3625f37875c15029cff
Nikita <latinisnotdead07@gmail.com>
2020-07-28 15:00:49 +0100

    Adds calculation constant to plane Calculator

--- modules/planeCalculator.js
+++ modules/planeCalculator.js
@@ -1,1 +1,6 @@
-const busCalculate = require('./busCalculator');
+function planeCalculate(distance) {
+    var raw_emission = distance * (0.25493 / 1000);
+    var emission = raw_emission.toFixed(2)
+    return `${emission} kg CO2e`
+  }
+module.exports = planeCalculate;
