e7d76f56fe4ea0d868870ae9ae06cef4ff414e26
Nick Butler <nwmbutler@gmail.com>
2020-07-29 10:51:39 +0100

    Kettle boils equivalent added to all forms of transport

--- modules/carCalculator.js
+++ modules/carCalculator.js
@@ -2,7 +2,8 @@ function carCalculate(distance, mode) {
   if (mode == 'driving') {
     var raw_emission = distance * (0.19228 / 1000);
     var emission = raw_emission.toFixed(2)
-    return `${emission} kg CO2e`
+    var kettle = Math.round(emission * 1000 / 70)
+    return `${emission} kg CO2e which is equivalent to ${kettle} kettle boils! `
   } else {
     return `0 kg CO2e`;
   }
--- modules/trainCalculator.js
+++ modules/trainCalculator.js
@@ -1,7 +1,8 @@
 function trainCalculate(distance) {
     var raw_emission = distance * (0.04115 / 1000);
     var emission = raw_emission.toFixed(2)
-    return `${emission} kg CO2e`
+    var kettle = Math.round(emission * 1000 / 70)
+    return `${emission} kg CO2e which is equivalent to ${kettle} kettle boils! `
   }
 module.exports = trainCalculate;
 
