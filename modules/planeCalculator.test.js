61c4c0ce47117cd61fd43d011c7c47f6c2a56736
Nick Butler <nwmbutler@gmail.com>
2020-07-29 11:07:30 +0100

    all tests passing

--- modules/planeCalculator.test.js
+++ modules/planeCalculator.test.js
@@ -1,11 +1,11 @@
 const planeCalculate = require('./planeCalculator');
 
-test('returns emissions for a train journey', () => {
-  var emissions = "0.25 kg CO2e";
+test('returns emissions for a plane journey', () => {
+  var emissions = "0.25 kg CO2e which is equivalent to 4 kettle boils! ";
   expect(planeCalculate(1000)).toBe(emissions);
 });
 
-test('returns emissions for a train journey', () => {
-  var emissions = "25.49 kg CO2e";
+test('returns emissions for a plane journey', () => {
+  var emissions = "25.49 kg CO2e which is equivalent to 364 kettle boils! ";
   expect(planeCalculate(100000)).toBe(emissions);
 });
