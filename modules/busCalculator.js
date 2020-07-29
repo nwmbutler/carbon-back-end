255a8df43c650564781f8cf9bc6ebe866c0c103c
Nick Butler <nwmbutler@gmail.com>
2020-07-29 17:56:04 +0100

    kettle logic updated

--- modules/busCalculator.js
+++ modules/busCalculator.js
@@ -1,7 +1,7 @@
 function busCalculate(distance) {
     var raw_emission = distance * (0.02779 / 1000);
     var emission = raw_emission.toFixed(2)
-    var kettle = Math.round(emission * 1000 / 70)
+    var kettle = Math.round(emission * 1000 / 15)
     return `${emission} kg CO2e which is equivalent to ${kettle} kettle boils! `
   }
 module.exports = busCalculate;
--- modules/carCalculator.js
+++ modules/carCalculator.js
@@ -2,7 +2,7 @@ function carCalculate(distance, mode) {
   if (mode == 'driving') {
     var raw_emission = distance * (0.19228 / 1000);
     var emission = raw_emission.toFixed(2)
-    var kettle = Math.round(emission * 1000 / 70)
+    var kettle = Math.round(emission * 1000 / 15)
     return `${emission} kg CO2e which is equivalent to ${kettle} kettle boils! `
   } else {
     return `0 kg CO2e`;
